f2920c4b294b09c6c0b036714720f6c2c6f1182f
nephros <nemo@JollaC2>
2025-01-07 11:23:03 +0100

    Translations: update German

--- gui/translations/systemd-journal-viewer-de.ts
+++ gui/translations/systemd-journal-viewer-de.ts
@@ -4,128 +4,188 @@
 <context>
     <name>CoverPage</name>
     <message>
-        <source>Journal Log</source>
-        <translation type="unfinished"></translation>
+        <source>Journal Viewer</source>
+        <translation>Journal Viewer</translation>
     </message>
 </context>
 <context>
     <name>FileBrowser</name>
     <message>
         <source>Save here</source>
-        <translation type="unfinished"></translation>
+        <translation>Hier speichern</translation>
     </message>
     <message>
         <source>No subdirectories</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Unterverzeichisse</translation>
     </message>
 </context>
 <context>
     <name>FirstPage</name>
     <message>
         <source>Journal Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>Journal Viewer</translation>
     </message>
     <message>
         <source>Copy to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>In die Zwischenablage</translation>
     </message>
     <message>
         <source>Log saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Log unter %1 gespeichert</translation>
     </message>
     <message>
         <source>Save log to file</source>
-        <translation type="unfinished"></translation>
+        <translation>Log als Datei speichern</translation>
+    </message>
+    <message>
+        <source>Save journal database</source>
+        <translation>Journaldatenbank speichern</translation>
+    </message>
+    <message>
+        <source>Journal saved to Documents</source>
+        <translation>Journal unter Dokumente gespeichert</translation>
+    </message>
+    <message>
+        <source>Waiting for messages…</source>
+        <translation>Warte auf Einträge…</translation>
+    </message>
+    <message>
+        <source>Swipe right to adjust filters.</source>
+        <translation>Wische nach rechts zum Anpassen der Filter</translation>
     </message>
 </context>
 <context>
     <name>OptionsPage</name>
     <message>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Optionen</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Entfernene</translation>
     </message>
     <message>
         <source>Add match filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtermuster hinzufügen</translation>
     </message>
     <message>
         <source>Syslog identifier</source>
-        <translation type="unfinished"></translation>
+        <translation>Syslog-Id</translation>
     </message>
     <message>
         <source>Exact message</source>
-        <translation type="unfinished"></translation>
+        <translation>Genauer Eintrag</translation>
     </message>
     <message>
         <source>Custom match rule</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Clear matches</source>
-        <translation type="unfinished"></translation>
+        <translation>Benutzerdefiniert</translation>
     </message>
     <message>
         <source>Apply changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Änderungen anwenden</translation>
     </message>
     <message>
         <source>Clear log</source>
-        <translation type="unfinished"></translation>
+        <translation>Log leeren</translation>
     </message>
     <message>
         <source>Executable path</source>
-        <translation type="unfinished"></translation>
+        <translation>Binärdateipfad</translation>
     </message>
     <message>
         <source>Click to add match filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Tippen zum Hinzufügen</translation>
     </message>
     <message>
         <source>Load previous messages</source>
-        <translation type="unfinished"></translation>
+        <translation>Frühere Einträge laden</translation>
     </message>
     <message>
         <source>N last messages</source>
-        <translation type="unfinished"></translation>
+        <translation>Die N letzten Einträge</translation>
     </message>
     <message>
         <source>Messages since date</source>
-        <translation type="unfinished"></translation>
+        <translation>Einträge seit</translation>
     </message>
     <message>
         <source>Load N last messages</source>
-        <translation type="unfinished"></translation>
+        <translation>Die N letzten Einträge laden</translation>
     </message>
     <message>
         <source>Select date and time</source>
-        <translation type="unfinished"></translation>
+        <translation>Wähle Zeit und Datum</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <source>Filtering</source>
+        <translation>Filtern</translation>
+    </message>
+    <message>
+        <source>Log Level</source>
+        <translation>Log-Level</translation>
+    </message>
+    <message>
+        <source>Transport</source>
+        <translation>Transport</translation>
+    </message>
+    <message>
+        <source>Click to add transport filter</source>
+        <translation>Tippen zum Hinzufügen</translation>
+    </message>
+    <message>
+        <source>Journal</source>
+        <translation>Journal</translation>
+    </message>
+    <message>
+        <source>Syslog</source>
+        <translation>Syslog</translation>
+    </message>
+    <message>
+        <source>Standard Output</source>
+        <translation>Standard-Out</translation>
+    </message>
+    <message>
+        <source>Kernel</source>
+        <translation>Kernel</translation>
+    </message>
+    <message>
+        <source>Driver</source>
+        <translation>Treiber</translation>
+    </message>
+    <message>
+        <source>Command name</source>
+        <translation>Kommandoname</translation>
+    </message>
+    <message>
+        <source>Clear filters</source>
+        <translation>Filterliste leeren</translation>
+    </message>
+    <message>
+        <source>Filter List</source>
+        <translation>Filterliste</translation>
     </message>
 </context>
 <context>
     <name>WelcomePage</name>
     <message>
         <source>System Journal</source>
-        <translation type="unfinished"></translation>
+        <translation>System Journal</translation>
     </message>
     <message>
         <source>Read logs</source>
-        <translation type="unfinished"></translation>
+        <translation>Lognachrichten ansehen</translation>
     </message>
     <message>
         <source>Save journal</source>
-        <translation type="unfinished"></translation>
+        <translation>Journal speichern</translation>
     </message>
     <message>
         <source>Journal saved to Documents!</source>
-        <translation type="unfinished"></translation>
+        <translation>Journal unter Dokumente gespeichert!</translation>
     </message>
 </context>
 </TS>
